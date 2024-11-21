7f3f6e0cac2a2b8193bc2d57bbd858d7f42bcfb7
Maricieloaf02 <maricielo.af02@gmail.com>
2024-11-21 05:49:55 -0500

    feat: agregar configuración inicial del proyecto y estructura de archivos

---
+++ EdinRose/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ShopPage from './pages/ShopPage/ShopPage';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <ShopPage />
+    </div>
+  );
+};
+
+export default App;
