66c0a4f477b03a7b0af58ae404c4d072603d89b1
Maricieloaf02 <maricielo.af02@gmail.com>
2024-11-23 23:31:05 -0500

    chore: eliminación de carpeta no utilizada para optimizar estructura del proyecto
    Se eliminó la carpeta DE-TODITO que no estaba en uso para mantener la estructura del proyecto limpia. Este cambio asegura que solo queden las carpetas necesarias para la revisión y el mantenimiento del proyecto.

--- DE-TODITO/src/types/Event.ts
+++
@@ -1,7 +0,0 @@
-import { Product } from "./Product";
-
-declare global {
-  interface DocumentEventMap {
-    "add-to-cart": CustomEvent<{ product: Product }>;
-  }
-}
--- DE-TODITO/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  root: './src', // Directorio raíz
-  build: {
-    outDir: '../dist' // Directorio de salida
-  }
-});
